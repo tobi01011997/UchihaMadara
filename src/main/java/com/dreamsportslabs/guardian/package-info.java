9d3dc14a86e46387a5f0d1b479c2826ce690c228
tobi01011997 <tobi01011997@gmail.com>
2025-07-02 12:42:47 +0530

    adjust

--- src/main/java/com/dreamsportslabs/guardian/package-info.java
+++
@@ -1,4 +0,0 @@
-@ModuleGen(groupPackage = "com.dreamsportslabs.guardian", name = "Guardian")
-package com.dreamsportslabs.guardian;
-
-import io.vertx.codegen.annotations.ModuleGen;
--- src/main/java/com/dreamsportslabs/guardian/service/IdProvider.java
+++
@@ -1,8 +0,0 @@
-package com.dreamsportslabs.guardian.service;
-
-import io.reactivex.rxjava3.core.Single;
-import io.vertx.core.json.JsonObject;
-
-public interface IdProvider {
-  Single<JsonObject> getUserIdentity(String authorizationToken);
-}
